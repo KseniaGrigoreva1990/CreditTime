086b8218df7f033d24a49d493405acb6ba67f550
Ksenia Grigoreva <grigoreva10.90@gmail.com>
2022-08-12 18:19:01 +0300

    credit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        CreditPaymentService program = new CreditPaymentService();
+        CreditPaymentService creditProgram = new CreditPaymentService();
 
-        double payment = program.calculate(1000000, 9.99, 12);
+        double payment = creditProgram.calculate(1000000, 9.99, 12);
         System.out.println(Math.round(payment));
 
-        double payment2 = program.calculate(1000000, 9.99, 24);
+        double payment2 = creditProgram.calculate(1000000, 9.99, 24);
         System.out.println(Math.round(payment2));
 
-        double payment3 = program.calculate(1000000, 9.99, 36);
+        double payment3 = creditProgram.calculate(1000000, 9.99, 36);
         System.out.println(Math.round(payment3));
     }
 
