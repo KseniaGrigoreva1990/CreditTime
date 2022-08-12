1760bfd6546621457fad1e046c6530bf878da2e5
Ksenia Grigoreva <grigoreva10.90@gmail.com>
2022-08-12 17:22:31 +0300

    helpmeplease

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+        int payment = service.calculate(1000000,9.99,12);
+        System.out.println(payment);
+    }
+}
