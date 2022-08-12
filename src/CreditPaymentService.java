0bf86b37fa9e56a047b6b51258bbac2bd2d573af
Ksenia Grigoreva <grigoreva10.90@gmail.com>
2022-08-12 17:25:04 +0300

    helpMePlease

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,13 +1,7 @@
 public class CreditPaymentService {
-    public int calculate(int summa, double perYear, int period) {
-
-
-        double perMonth = (perYear /100) /  12;  //процент в месяц
-        //разбила формулу на части для простоты
-        double y = 1 + perMonth;
-        double x = Math.pow(y, period);
-        double payment = summa * (perMonth * x) / x - 1;
-        return (int) payment;
+    public double calculate(int summa, double perYear, int period) {
+        double perMonth = (perYear / 12) / 100;
+        double payment = summa * (perMonth * Math.pow(1.0 + perMonth, period)) / Math.pow(1.0 + perMonth, period) - 1;
+        return payment;
     }
 }
-
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        CreditPaymentService service = new CreditPaymentService();
-        int payment = service.calculate(1000000,9.99,12);
-        System.out.println(payment);
+        CreditPaymentService program = new CreditPaymentService();
+
+        double payment = program.calculate(1000000, 9.99, 12);
+        System.out.println(Math.round(payment));
+
+        double payment2 = program.calculate(1000000, 9.99, 24);
+        System.out.println(Math.round(payment2));
+
+        double payment3 = program.calculate(1000000, 9.99, 36);
+        System.out.println(Math.round(payment3));
     }
+
+
 }
