afeb2ed14546115dd8b8c70bbbbfdc9e681572d6
Ksenia Grigoreva <grigoreva10.90@gmail.com>
2022-08-12 18:12:05 +0300

    credit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
     public double calculate(int summa, double perYear, int period) {
         double perMonth = (perYear / 12) / 100;
-        double payment = summa * (perMonth * Math.pow(1.0 + perMonth, period)) / Math.pow(1.0 + perMonth, period) - 1;
+        double payment = summa * ((perMonth * Math.pow(1.0 + perMonth, period)) / (Math.pow(1.0 + perMonth, period) - 1));
         return payment;
     }
 }
